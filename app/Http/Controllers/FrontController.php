2dbb0fe6185087755d47916e2bde3e3263918971
manishkarki2022 <np03cs4s210031@heraldcollege.edu.np>
2024-04-19 08:17:19 +0545

    Add Highlight in frontController to add Auhtor in frontend

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ContactEmail;
+use App\Models\Author;
+use App\Models\Highlight;
 use App\Models\Page;
 use App\Models\Product;
 use App\Models\Wishlist;
@@ -13,14 +15,15 @@ use Illuminate\Support\Facades\Mail;
 
 class FrontController extends Controller
 {
-public function index(){
-    $latest=  Product::latest()
-        ->where('status',1)
-        ->take(6)
-        ->get();
-   $getFeatured=  Product::where('is_featured','Yes')->get();
-   return view('front.home',compact('getFeatured','latest'));
-    }
+    public function index(){
+        $latest=  Product::latest()
+            ->where('status',1)
+            ->take(6)
+            ->get();
+       $getFeatured=  Product::where('is_featured','Yes')->get();
+       $highlights = Highlight::where('is_active',1)->get();
+       return view('front.home',compact('getFeatured','latest','highlights'));
+        }
     public function addWishlist(Request $request){
 
         if(Auth::check()==false){
@@ -54,32 +57,45 @@ public function index(){
         }
         return view('front.page',compact('page'));
     }
-    public function sendContactEmail(Request $request){
+    public function sendContactEmail(Request $request)
+    {
         $validate = $request->validate([
             'name' => 'required',
             'email' => 'required|email',
             'subject' => 'required',
             'message' => 'required',
         ]);
-       if($validate){
-           $mailData = [
-               'name' => $request->name,
-               'email' => $request->email,
-               'subject' => $request->subject,
-               'message' => $request->message,
-               'mail_subject' => 'Contact Email'
-           ];
-           $admin = env('MAIL_FROM_ADDRESS');
-              Mail::to($admin)->send(new ContactEmail($mailData));
-              return redirect()->back()->with('success','Email sent successfully');
-       }else{
-           return redirect()->back()->with('error','Please fill all fields');
-       }
+        if ($validate) {
+            $mailData = [
+                'name' => $request->name,
+                'email' => $request->email,
+                'subject' => $request->subject,
+                'message' => $request->message,
+                'mail_subject' => 'Contact Email'
+            ];
+            $admin = env('MAIL_FROM_ADDRESS');
+            Mail::to($admin)->send(new ContactEmail($mailData));
+            return redirect()->back()->with('success', 'Email sent successfully');
+        } else {
+            return redirect()->back()->with('error', 'Please fill all fields');
+        }
+    }
 
-       }
-
+    public function authorIndex(){
+        $authors = Author::where('status','active')->get();
+        return view('front.author.index',compact('authors'));
+    }
+    public function authorShow($id){
+        $author = Author::findOrFail($id);
+        if(!$author){
+            abort(404);
+        }
+        $relatedProducts = Product::where('author_id',$id)->get();
+        return view('front.author.show',compact('author','relatedProducts'));
 
     }
+
+}
 
 
 
